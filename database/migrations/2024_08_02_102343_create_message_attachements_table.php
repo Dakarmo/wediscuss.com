f8b3f681f11b8eaf1b94f49839857d8583b0bef5
Armel K. Dovenon <dove.armel@gmail.com>
2024-08-02 12:06:20 +0100

    création de la migration message_attachements

--- database/migrations/2024_08_02_102343_create_message_attachements_table.php
+++ database/migrations/2024_08_02_102343_create_message_attachements_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('message_attachements', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('message_id')->constrained('messages');
+            $table->string('name', 255); //x2DdkhhUkdfhj.jpg
+            $table->string('path',  1024); // /image/x2DdkhhUkdfhj.jpg
+            $table->string('mime', 255); //jpg, png, xsl
+            $table->integer('seize'); //jpg, png, xsl
             $table->timestamps();
         });
     }
